65d26e1ff3f950c56e8ebd2370a62e8dc31095e2
Subhodeep Dey <90602098+subhodeep-dey@users.noreply.github.com>
2024-08-15 18:49:42 +0530

    Update connectDatabase.js

--- Backend/Helpers/database/connectDatabase.js
+++ Backend/Helpers/database/connectDatabase.js
@@ -1,11 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const connectDatabase =async  () => {
+mongoose.set('strictQuery', true);
 
-    await mongoose.connect(process.env.MONGO_URI ,{useNewUrlParser : true})
-
-    console.log("MongoDB Connection Successfully")
-
+const connectDatabase = async () => {
+    await mongoose.connect(process.env.MONGO_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        serverSelectionTimeoutMS: 5000,
+    });
+    console.log("MongoDB Connection Successfully");
 }
 
-module.exports = connectDatabase
+module.exports = connectDatabase;
